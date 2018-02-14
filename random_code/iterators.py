81e9c41345f078e01ead5e48cdb9c19dd4b68913
Stephen J Burke <steburke71@hotmail.com>
2018-02-14 16:05:11 -0500

    listStuff3

---
+++ random_code/iterators.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+__author__ = "Stephen J. Burke"
+__email__ = "steburke71@hotmail.com"
+__date__ = "2/14/18"
+__license__ = "GPL"
+
+numbers = "0123456789"
+
+iter1 = iter(numbers)
+
+
+#print(iter1)
+print(next(iter1))
+print(next(iter1))
+next(iter1)
+print(next(iter1))
---
+++ random_code/listStuff2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+__author__ = "Stephen J. Burke"
+__email__ = "steburke71@hotmail.com"
+__date__ = "2/14/18"
+__license__ = "GPL"
+
+even = [0, 2, 4, 6, 8]
+
+anotherEven = list(even)
+
+#anotherEven = even
+
+anotherEven.sort(reverse=True)
+
+print(even)
+print(anotherEven)
