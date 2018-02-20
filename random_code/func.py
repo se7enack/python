6f96c042990187e3dde7d2b3882f75d8ae9ca6ac
Stephen J. Burke <steburke71@hotmail.com>
2018-02-20 17:02:19 -0500

    ...

---
+++ random_code/func.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+__author__ = "Stephen J. Burke"
+__email__ = "steburke71@hotmail.com"
+__date__ = "2/14/18"
+__license__ = "GPL"
+
+
+def who(x, y):
+    print(x, end=" & ")
+    print(y)
+
+
+who('you', 'me')
