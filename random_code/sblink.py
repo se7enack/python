92a930c4dd28b394f57c722dbff5c690d954e80f
Stephen J. Burke <steburke71@hotmail.com>
2018-02-20 23:47:25 -0500

    sblink

--- random_code/sblink.py
+++ random_code/sblink.py
@@ -32,25 +32,25 @@ banner()
 
 choices = ((1, "Download all videos", "SB_TO_COME_BACK_TO"),
            (2, "Get network information", "/networks"),
-           (3, "Get Sync Module information", "/network/${NETWORKID}/syncmodules"),
-           (4, "Arm network", "/network/${NETWORKID}/arm"),
-           (5, "Disarm network", "/network/${NETWORKID}/disarm"),
+           (3, "Get Sync Module information", "/network/" + networkId + "/syncmodules"),
+           (4, "Arm network", "/network/" + networkId + "/arm"),
+           (5, "Disarm network", "/network/" + networkId + "/disarm"),
            (6, "Get homescreen information", "/homescreen"),
-           (7, "Get events for network", "/events/network/${NETWORKID}"),
-           (8, "Capture a new thumbnail", "/network/${NETWORKID}/camera/${CAMERAID}/thumbnail"),
-           (9, "Capture a new video", "/network/${NETWORKID}/camera/${CAMERAID}/clip"),
+           (7, "Get events for network", "/events/network/" + networkId + ""),
+           (8, "Capture a new thumbnail", "/network/" + networkId + "/camera/" + cameraId + "/thumbnail"),
+           (9, "Capture a new video", "/network/" + networkId + "/camera/" + cameraId + "/clip"),
            (10, "Get a total on the number of videos", "/api/v2/videos/count"),
            (11, "Get paginated video information", "SB_TO_COME_BACK_TO"),
            (12, "Unwatched video list", "/api/v2/videos/unwatched"),
-           (13, "Get a list of all cameras", "/network/${NETWORKID}/cameras"),
-           (14, "Get camera information", "/network/${NETWORKID}/camera/${CAMERAID}"),
-           (15, "Get camera sensor information", "/network/${NETWORKID}/camera/${CAMERAID}/signals"),
-           (16, "Enable motion detection", "/network/${NETWORKID}/camera/${CAMERAID}/enable"),
-           (17, "Disable motion detection", "/network/${NETWORKID}/camera/${CAMERAID}/disable"),
+           (13, "Get a list of all cameras", "/network/" + networkId + "/cameras"),
+           (14, "Get camera information", "/network/" + networkId + "/camera/" + cameraId),
+           (15, "Get camera sensor information", "/network/" + networkId + "/camera/" + cameraId + "/signals"),
+           (16, "Enable motion detection", "/network/" + networkId + "/camera/" + cameraId + "/enable"),
+           (17, "Disable motion detection", "/network/" + networkId + "/camera/" + cameraId + "/disable"),
            (18, "Get information about connected devices", "/account/clients"),
            (19, "Get information about supported regions", "/regions"),
            (20, "Get information about system health", "/health"),
-           (21, "Get information about programs", "/api/v1/networks/${NETWORKID}/programs")), 'exit'
+           (21, "Get information about programs", "/api/v1/networks/" + networkId + "/programs")), 'exit'
 options, bye = choices
 maxSize = len(options)
 
