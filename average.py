92606c9d71f4ec43b170df2f5c28a20709ff66f0
Stephen Burke <se7enack@users.noreply.github.com>
2019-04-28 16:24:28 -0400

    Update average.py

--- average.py
+++ average.py
@@ -1,17 +1,17 @@
 #!/bin/env python3
-x = 1
-y = 0
-counter = 0
-total = 0
+x = 1  # Start point (number is ignored, just needs to be more than 0)
+counter = 0  # Used to calculate the average
+total = 0  # Needs to be zero to start the adding of future values
+
+print()
 
 try:
     while x > 0:
-        x = int(input('pick a number:\t'))
+        x = int(input(str(counter + 1) + ') Enter a number. (enter 0 or leave blank if done):\t'))
         counter += 1
-        total = x + y
-        y = total
+        total = x + total
         if counter > 1 and x > 0:
-            print(total / counter)
+            print('average so far: ' + str(total / counter) + '\n')
         elif x == 0:
             counter -= 1
 except ValueError:
